ff7a6fbc5637304d497a6d917693cf025eaf422c
siti nurviatika <sitinurviatika@gmail.com>
2025-04-17 16:36:38 +0700

    perbaikan vite

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ export default {
       rollupOptions: {
         input: {
           main: 'index.html',
-          about: 'src/about.html',
-          blog: 'src/blog.html',
-          blogDetail:'src/blogDetail.html',
-          contactUs: 'src/contactUs.html',
-          features: 'src/features.html',
-          pricing: 'src/pricing.html',
+          about: '/src/about.html',
+          blog: '/src/blog.html',
+          blogDetail:'/src/blogDetail.html',
+          contactUs: '/src/contactUs.html',
+          features: '/src/features.html',
+          pricing: '/src/pricing.html',
         }
       }
     }
