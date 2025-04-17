97682ef9fdc8826cca3d227cdb6282ab0f77bac5
siti nurviatika <sitinurviatika@gmail.com>
2025-04-17 16:46:18 +0700

    perbaikan vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-// export default {
-//     build: {
-//       rollupOptions: {
-//         input: {
-//           main: 'index.html',
-//           about: '/src/about.html',
-//           blog: '/src/blog.html',
-//           blogDetail:'/src/blogDetail.html',
-//           contactUs: '/src/contactUs.html',
-//           features: '/src/features.html',
-//           pricing: '/src/pricing.html',
-//         }
-//       }
-//     }
-//   }
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "/src/about.html",
+        blog: "/src/blog.html",
+        blogDetail: "/src/blogDetail.html",
+        contactUs: "/src/contactUs.html",
+        features: "/src/features.html",
+        pricing: "/src/pricing.html",
+      },
+    },
+  },
+};
