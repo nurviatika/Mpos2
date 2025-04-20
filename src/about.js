b9ea4f552e870d5e9c0b0b935601fef47a1c4f5c
siti nurviatika <sitinurviatika@gmail.com>
2025-04-20 21:46:01 +0700

    perbaikan page js

--- src/about.js
+++ src/about.js
@@ -21,4 +21,19 @@ document.addEventListener("DOMContentLoaded", function () {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  let lastScrollTop = 0;
+  const navbar = document.getElementById("navbar");
+
+  window.addEventListener("scroll", function () {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      navbar.style.transform = "translateY(-100%)";
+    } else {
+      navbar.style.transform = "translateY(0)";
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  });
 })
