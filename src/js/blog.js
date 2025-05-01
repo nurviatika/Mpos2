7c978e584830c5e6be4856e5bc6db3e0ed656924
siti nurviatika <sitinurviatika@gmail.com>
2025-05-01 10:23:31 +0700

    perbaikan container & page blog

---
+++ src/js/blog.js
@@ -0,0 +1,184 @@
+const blogs = [
+  {
+    id: 1,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame1.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 2,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame2.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 3,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame3.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 4,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame4.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 5,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame1.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 6,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame5.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 7,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame1.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 8,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame2.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 9,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame3.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 10,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame4.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 11,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame1.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+  {
+    id: 12,
+    title: "Effective Strategies for Streamlining Inventory Management in Small Restaurants",
+    img: "/blog-frame5.png",
+    author: "Damar Buana",
+    category: "Business Insights",
+    created_at: "8 min read",
+  },
+ 
+];
+
+const blogContainer = document.getElementById("bloglist");
+const paginationContainer = document.getElementById("pagination");
+
+const itemsPerPage = 6;
+let currentPage = 1;
+
+function displayBlogs(page) {
+  blogContainer.innerHTML = "";
+  const start = (page - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const paginatedBlogs = blogs.slice(start, end);
+
+  paginatedBlogs.forEach((blog) => {
+    blogContainer.innerHTML += `
+      <div
+        data-aos="fade-up"
+        data-aos-duration="700"
+        data-aos-delay="0"
+        data-aos-mirror="true"
+        data-aos-once="false"
+        class="flex-col gap-5 flex mb-10"
+      >
+        <img src="${blog.img}" alt="blog image ${blog.id}" class="rounded-lg" />
+        <div class="md:max-w-[368px] lg:max-w-[380px] max-w-[368px] w-full font-jakarta">
+          <h3 class="text-base font-medium font-satoshi leading-5 pt-[15px] pb-[8px]">
+            ${blog.title}
+          </h3>
+          <p class="text-abuGelap md:text-sm text-xs">
+            By ${blog.author} | ${blog.category} | ${blog.created_at}
+          </p>
+        </div>
+      </div>`;
+  });
+}
+
+function setupPagination() {
+  paginationContainer.innerHTML = "";
+  const pageCount = Math.ceil(blogs.length / itemsPerPage);
+
+  const prevBtn = document.createElement("button");
+  prevBtn.innerHTML = "←";
+  prevBtn.className = "w-7 h-7 border-none hover:border rounded-full text-sm hover:bg-Biru hover:text-white";
+  prevBtn.disabled = currentPage === 1;
+  prevBtn.addEventListener("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      displayBlogs(currentPage);
+      setupPagination();
+    }
+  });
+  paginationContainer.appendChild(prevBtn);
+
+  for (let i = 1; i <= pageCount; i++) {
+    const pageButton = document.createElement("button");
+    pageButton.innerText = i;
+    pageButton.className = `w-7 h-7 flex items-center justify-center rounded-full border-none hover:border text-sm ${
+      i === currentPage
+        ? "bg-Biru text-white"
+        : "bg-putih_bg text-hitam hover:bg-Biru hover:text-white"
+    }`;
+    pageButton.addEventListener("click", () => {
+      currentPage = i;
+      displayBlogs(currentPage);
+      setupPagination();
+    });
+    paginationContainer.appendChild(pageButton);
+  }
+
+  const nextBtn = document.createElement("button");
+  nextBtn.innerHTML = "→";
+  nextBtn.className = "w-7 h-7 border-none hover:border rounded-full text-sm hover:bg-Biru hover:text-white";
+  nextBtn.disabled = currentPage === pageCount;
+  nextBtn.addEventListener("click", () => {
+    if (currentPage < pageCount) {
+      currentPage++;
+      displayBlogs(currentPage);
+      setupPagination();
+    }
+  });
+  paginationContainer.appendChild(nextBtn);
+}
+
+displayBlogs(currentPage);
+setupPagination();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.{html,js}",
-    "./src/**/*.{html,js}"],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     container: {
       center: true,
-      padding: "1.5rem",
+      padding: {
+        sm: "16px",
+        md: "24px",
+        lg: "16px",
+      },
+      // padding: "1.5rem",
     },
     extend: {
       fontFamily: {
-      inter: ["Inter", "sans-serif"],
-      poppins: ["Poppins", "sans-serif"],
-      jakarta: ["Plus Jakarta Sans", "sans-serif"],
-      helvetica: ["Helvetica", "sans-serif"],
-      satoshi: ["Satoshi", "sans-serif"],
-      lato: ["Lato", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+        helvetica: ["Helvetica", "sans-serif"],
+        satoshi: ["Satoshi", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
       },
       colors: {
         hitam: "#050810",
@@ -35,17 +38,16 @@ export default {
         abu_bg: "#F8F9FA",
         hitam_text: "#344346",
         merah: "#FF8682",
+        gray: "#D9D9D9",
       },
       screens: {
-        sm: "425px",
-        smd: "568px",
+        sm: "375px",
         md: "768px",
-        mdlg: "990px",
         lg: "1024px",
         xl: "1200px",
+        "2xl": "1200px",
       },
     },
   },
   plugins: [],
-}
-
+};
